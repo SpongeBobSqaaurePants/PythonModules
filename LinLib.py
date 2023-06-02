329b2201a4823785776e39302eca490a6500df31
SpongeBobSqaaurePants <135346015+SpongeBobSqaaurePants@users.noreply.github.com>
2023-06-02 20:05:50 +0530

    Add files via upload
    You should you from __ import to use the functions. but first should implement the file in your code. 
    I have used the facility in python to make the function.
    And Yeah That's It.
    Enjoy!

---
+++ LinLib.py
@@ -0,0 +1,45 @@
+
+# Copyright © 2023 LinLib
+
+import time
+
+def var():
+    INT = int
+    STRING = str
+    FLOAT = float
+class functions:
+    def display(STR):
+        print(STR)
+class BasicMaths:
+    def additions(x, y):
+        Sum = int(x) + int(y)
+        print("""
+Processing please wait...
+        """)
+        time.sleep(2.5)
+        print("The number is", Sum)
+    def subtract(x, y):
+        Sum = int(x) - int(y)
+        print("""
+Processing please wait...
+        """)
+        time.sleep(2.5)
+        print("The number is", Sum)
+    def multiplaction(x, y):
+        Sum = int(x) * int(y)
+        print("""
+Processing please wait...
+        """)
+        time.sleep(2.5)
+        print("The number is", Sum)
+    def division(x, y):
+        Sum = float(x) / float(y)
+        print("""
+Processing please wait...
+        """)
+        time.sleep(2.5)
+        print("The number is", Sum)
+
+# I will be adding more function in the future
+
+
